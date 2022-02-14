de55b6e84f892dc2df7bd539c93c7d84b6f9dd02
carlos.silva <carlos.silva@bbts.com.br>
2022-02-14 11:28:24 -0300

    Implementação da utilização do RabbitMQ para fila de votações.

--- src/main/java/com/example/teste/entities/StatusCPF.java
+++ src/main/java/com/example/teste/entities/StatusCPF.java
@@ -2,8 +2,6 @@ package com.example.teste.entities;
 
 public class StatusCPF {
 
-    private Integer code;
-    private Boolean isError;
     private String status;
 
     public StatusCPF(){
@@ -16,21 +14,5 @@ public class StatusCPF {
 
     public void setStatus(String status) {
         this.status = status;
-    }
-
-    public Integer getCode() {
-        return code;
-    }
-
-    public void setCode(Integer code) {
-        this.code = code;
-    }
-
-    public Boolean getError() {
-        return isError;
-    }
-
-    public void setError(Boolean error) {
-        isError = error;
     }
 }
